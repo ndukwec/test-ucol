ab3b76481a0c1849e5689fcb5c6830d067c25682
ndukwec <chinedua@umich.edu>
2019-03-09 16:25:29 -0500

    renamed test method

--- testingidentityfunc.py
+++ testingidentityfunc.py
@@ -5,7 +5,7 @@ import identityfunc
 class TestingMethodsInOurProject(unittest.TestCase):
 
     # Our test method below will call our identity function to verify its output
-    def test_method_returns_a_type_of_tuple(self):
+    def test_method_to_verify_id_func_returns_a_type_of_tuple(self):
         ret_value_from_idfunc = identityfunc.id_func("hi there")
         self.assertEqual(ret_value_from_idfunc, ("hi there",))
 
