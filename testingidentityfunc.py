475ff159fbbda9e065e5b61b6f0cff39ec6a4dbf
ndukwec <chinedua@umich.edu>
2019-03-09 16:13:44 -0500

    added test for the id function

---
+++ testingidentityfunc.py
@@ -0,0 +1,12 @@
+import unittest
+import identityfunc
+
+
+class TestingMethodsInOurProject(unittest.TestCase):
+
+    # Our test method below will call our identity function to verify its output
+    def test_method_returns_a_type_of_tuple(self):
+        ret_value_from_idfunc = identityfunc.id_func("hi there")
+        self.assertEqual(ret_value_from_idfunc, ("hi there",))
+
+
